07fd76b75e752cdca5a8aeff0576cdefc58158cd
ManausK <1905246@kiit.ac.in>
2023-05-25 23:26:53 +0530

    final changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,55 +1,116 @@
-import React from "react";
+import { React, useState } from "react";
 import DownloadAds from "./DownloadAds";
+import VisibilitySensor from "react-visibility-sensor";
+import { motion } from "framer-motion";
 
 function Hero() {
+  const [elementIsVisible, setElementIsVisible] = useState(false);
+  const bg = {
+    true: {
+      left: "7rem",
+    },
+    false: {
+      left: "19rem",
+    },
+  };
+  const musicPlayer = {
+    true: {
+      left: "295px",
+    },
+    false: {
+      left: "235px",
+    },
+  };
+  const rect = {
+    true: {
+      left: "11rem",
+    },
+    false: {
+      left: "13rem",
+    },
+  };
+  const heart = {
+    true: {
+      left: "9rem",
+    },
+    false: {
+      left: "12.5rem",
+    },
+  };
   return (
-    <div className="wrapper bg-[#081730] flex items-center justify-between px-[5rem] w-[100%] rounded-b-[5rem] h-[35rem] relative z-[3]">
-      {/* left side */}
-      <div className="headings flex flex-col items-start justify-center h-[100%] text-[3rem]">
-        <span>Experience The</span>
-        <span>
-          <b>Best Quality Music</b>
-        </span>
-        <span className="text-[15px] text-[#525d6e]">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, neque.
-          <br />
-          Lorem ipsum dolor sit amet consectetur adipisicing.
-        </span>
-        {/* download ads */}
-        <div>
-          <span className="text-[13px]">Download now on IOS and Android</span>
+    <VisibilitySensor
+      onChange={(isVisible) => setElementIsVisible(isVisible)}
+      minTopValue={300}
+    >
+      <div className="wrapper bg-[#081730] flex items-center justify-between px-[5rem] rounded-b-[5rem] w-[100%] h-[35rem] relative z-[3]">
+        {/* left side */}
+        <div className="headings flex flex-col items-start justify-center h-[100%] text-[3rem]">
+          <span>Experience The</span>{" "}
+          <span>
+            <b>Best Quality Music</b>
+          </span>
+          <span className="text-[15px] text-[#525D6E]">
+            Donec laoreet nec velit vitae aliquam. Ut quis tincidunt purus.
+            <br />
+            Suspendisse in leo non risus tincidunt lobortis.
+          </span>
+          {/* download ads */}
+          <div>
+            <span className="text-[13px]">Download now on IOS and Android</span>
+            <DownloadAds />
+          </div>
         </div>
-        <DownloadAds />
+        {/* right side */}
+        <div className="images relative w-[50%]">
+          <motion.img
+            variants={bg}
+            animate={`${elementIsVisible}`}
+            transition={{ duration: 1, type: "ease-out" }}
+            src={require("../img/backgraphics.png")}
+            alt=""
+            className="absolute top-[-8rem] left-[19rem]"
+          />
+          <img
+            src={require("../img/p 1.png")}
+            alt=""
+            className="absolute top-[-15rem] h-[34rem] left-[13rem]"
+          />
+          <motion.img
+            variants={musicPlayer}
+            animate={`${elementIsVisible}`}
+            transition={{
+              duration: 1,
+              type: "ease-out",
+            }}
+            src={require("../img/p 2.png")}
+            alt=""
+            className="absolute left-[235px] top-[94px] w-[175px]"
+          />
+          <motion.img
+            variants={rect}
+            animate={`${elementIsVisible}`}
+            transition={{
+              type: "ease-out",
+              duration: 1,
+            }}
+            src={require("../img/p 3.png")}
+            alt=""
+            className="absolute w-[5rem] left-[13rem] top-[12rem]"
+          />
+          <motion.img
+            variants={heart}
+            animate={`${elementIsVisible}`}
+            transition={{
+              type: "ease-out",
+              duration: 1,
+            }}
+            src={require("../img/p 4.png")}
+            alt=""
+            className="absolute w-[5rem] left-[12.5rem] top-[12rem]"
+          />
+        </div>
       </div>
-      {/* right side */}
-      <div className="images relative w-[50%]">
-        <img
-          src={require("../img/backgraphics.png")}
-          alt="backgraphics"
-          className="absolute top-[-8rem] left-[19rem]"
-        />
-        <img
-          src={require("../img/p 1.png")}
-          alt="p1"
-          className="absolute top-[-15rem] h-[34rem] left-[13rem]"
-        />
-        <img
-          src={require("../img/p 2.png")}
-          alt="p2"
-          className="absolute top-[94px] w-[175px] left-[235px]"
-        />
-        <img
-          src={require("../img/p 3.png")}
-          alt="p3"
-          className="absolute top-[12rem] w-[5rem] left-[13rem]"
-        />
-        <img
-          src={require("../img/p 4.png")}
-          alt="p4"
-          className="absolute w-[5rem] left-[12.5rem] top-[12rem]"
-        />
-      </div>
-    </div>
+    </VisibilitySensor>
   );
 }
 
